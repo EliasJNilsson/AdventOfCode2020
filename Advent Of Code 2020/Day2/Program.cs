53409fe2005145004dbaa3cffab18a8244bb4acf
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-02 11:40:27 -0800

    Update the code for Part2.
    I am now starting to understand that they are generally 2 parters.. and I'll likely set this up a little different tomorrow.

--- Advent Of Code 2020/Day2/Program.cs
+++ Advent Of Code 2020/Day2/Program.cs
@@ -23,13 +23,34 @@ namespace Day2
                 }
 
                 //Do the checks
-                if(ValidatePassword(validationPasswordRequest))
+                if(ValidatePasswordPart1(validationPasswordRequest))
                 {
                     validCount++;
                 }
             }
 
-            Console.WriteLine($"The valid count of passwords is {validCount}");
+            Console.WriteLine($"The valid count of part 1 passwords is {validCount}");
+
+            int validCount2 = 0;
+
+            foreach (var line in splitText)
+            {
+                //split the line to the information needed
+                var validationPasswordRequest = GenerateValidationRequest(line);
+
+                if (validationPasswordRequest == null)
+                {
+                    continue;
+                }
+
+                //Do the checks
+                if (ValidatePasswordPart2(validationPasswordRequest))
+                {
+                    validCount2++;
+                }
+            }
+
+            Console.WriteLine($"The valid count of part 2 passwords is {validCount2}");
         }
 
         public static ValidationPasswordRequest GenerateValidationRequest(string input)
@@ -53,7 +74,7 @@ namespace Day2
             return returnOutput;
         }
 
-        public static bool ValidatePassword(ValidationPasswordRequest request)
+        public static bool ValidatePasswordPart1(ValidationPasswordRequest request)
         {
             //could also do group by, sort of splitting of the string, but to save braincells today.. I'm just going to loop and count.
 
@@ -69,6 +90,25 @@ namespace Day2
 
             if((countOfChar >= request.Range.lower) && (countOfChar <= request.Range.upper))
             {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool ValidatePasswordPart2(ValidationPasswordRequest request)
+        {
+            //This validation is not upper and lower of count, as much as the placement of the letters. I will still use the range object for simplicity in this event.
+
+            var letter1 = request.Password[request.Range.lower - 1];
+            var letter2 = request.Password[request.Range.upper - 1];
+
+            if((letter1 == letter2) && letter1 == request.Letter)
+            { 
+                return false; 
+            }
+
+            if(letter1 == request.Letter || letter2 == request.Letter)
+            { 
                 return true;
             }
             return false;
