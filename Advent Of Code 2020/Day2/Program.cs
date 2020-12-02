5687fd0b8118e0878e21db0d5a5cd41598fd2564
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-02 11:03:09 -0800

    Initialization Day2

---
+++ Advent Of Code 2020/Day2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string streamText = System.IO.File.ReadAllText(@"..\..\..\input.txt");
+
+            System.Console.WriteLine($"Content of input.txt = {streamText}");
+        }
+    }
+}
