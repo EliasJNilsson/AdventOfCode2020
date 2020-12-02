7e3c02aa90d79ea2d9e095f4e4901afb5d24ef13
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-02 11:32:11 -0800

    Adding in the actual Day2.pt1 code (correct answer)

--- Advent Of Code 2020/Day2/Program.cs
+++ Advent Of Code 2020/Day2/Program.cs
@@ -8,7 +8,83 @@ namespace Day2
         {
             string streamText = System.IO.File.ReadAllText(@"..\..\..\input.txt");
 
-            System.Console.WriteLine($"Content of input.txt = {streamText}");
+            var splitText = streamText.Split("\n");
+
+            int validCount = 0;
+
+            foreach(var line in splitText)
+            {
+                //split the line to the information needed
+                var validationPasswordRequest = GenerateValidationRequest(line);
+
+                if(validationPasswordRequest == null)
+                {
+                    continue;
+                }
+
+                //Do the checks
+                if(ValidatePassword(validationPasswordRequest))
+                {
+                    validCount++;
+                }
+            }
+
+            Console.WriteLine($"The valid count of passwords is {validCount}");
         }
+
+        public static ValidationPasswordRequest GenerateValidationRequest(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                return null;
+            }
+
+            var seperatedStrings = input.Split(" ");
+
+            var range = seperatedStrings[0].Split("-");
+
+            var returnOutput = new ValidationPasswordRequest()
+            {
+                Range = new Range() { lower = int.Parse(range[0]), upper = int.Parse(range[1]) },
+                Letter = seperatedStrings[1][0],
+                Password = seperatedStrings[2]
+            };
+
+            return returnOutput;
+        }
+
+        public static bool ValidatePassword(ValidationPasswordRequest request)
+        {
+            //could also do group by, sort of splitting of the string, but to save braincells today.. I'm just going to loop and count.
+
+            var countOfChar = 0;
+
+            foreach(var letter in request.Password)
+            {
+                if(letter == request.Letter)
+                {
+                    countOfChar++;
+                }
+            }
+
+            if((countOfChar >= request.Range.lower) && (countOfChar <= request.Range.upper))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class Range
+    {
+        public int lower { get; set; }
+        public int upper { get; set; }
+    }
+
+    public class ValidationPasswordRequest
+    {
+        public Range Range { get; set; }
+        public char Letter { get; set; }
+        public string Password { get; set; }
     }
 }
