4bb916ba508cc503774221778cd5bd5f837d46f0
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-03 12:13:58 -0800

    Day3 Pt1

---
+++ Advent Of Code 2020/Day3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string streamText = System.IO.File.ReadAllText(@"..\..\..\input.txt");
+
+            var splitText = streamText.Split("\n");
+
+            int col = splitText[0].Length;
+            var treeCount = 0;
+            int placement = 0;
+
+            foreach(var row in splitText)
+            {
+                //Part 1
+                placement += 3;
+                if(placement >= col)
+                {
+                    placement -= col;
+                }
+
+                if(IsThereTree(placement, row))
+                {
+                    treeCount++;
+                }
+            }
+
+            Console.WriteLine($"The count of how many trees hit is: {treeCount}");            
+
+        }
+
+        static bool IsThereTree(int col, string row)
+        {
+            if (row[col] == '#')
+            {
+                Console.WriteLine($"hit tree: {row[col]}");
+                return true;
+            }
+
+            Console.WriteLine($"clear snow: {row[col]}");
+            return false;
+        }
+    }
+}
