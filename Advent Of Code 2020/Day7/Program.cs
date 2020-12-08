e5160fe01671f294109f4d4fb3fe97fad3157639
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-08 15:22:27 -0800

    Made Day 7

---
+++ Advent Of Code 2020/Day7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
+        }
+    }
+}
