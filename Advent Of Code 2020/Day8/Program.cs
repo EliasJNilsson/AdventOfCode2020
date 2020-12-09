238cce8e972c6541eeac7068884cca1b4a7be652
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-09 15:51:12 -0800

    Day8 Part1 complete

--- Advent Of Code 2020/Day8/Program.cs
+++ Advent Of Code 2020/Day8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day8
 {
@@ -8,6 +9,62 @@ namespace Day8
         {
             var input = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
 
+            var passage = new Dictionary<int, bool>();
+
+            int line = 0;
+            int accumulator = 0;
+
+            while(!passage.ContainsKey(line))
+            {
+                passage.Add(line, true);
+
+                var currentLine = input[line];
+
+                switch (currentLine.Substring(0,3))
+                {
+                    case "acc":
+                        {
+                            var plusOrMinus = int.Parse(currentLine.Substring(5));
+
+                            if (currentLine[4] == '+')
+                            {
+                                accumulator += plusOrMinus;
+                            }
+                            else
+                            {
+                                accumulator -= plusOrMinus;
+                            }
+                            line++;
+                            break;
+                        }
+                    case "nop":
+                        {
+                            line++;
+                            break;
+                        }
+                    case "jmp":
+                        {
+                            var plusOrMinus = int.Parse(currentLine.Substring(5));
+
+
+                            if (currentLine[4] == '+')
+                            {
+                                line += plusOrMinus;
+                            }
+                            else
+                            {
+                                line -= plusOrMinus;
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+
+            Console.WriteLine($"We got out! doubled up on line {line}, and the accumulator is at {accumulator}");
         }
     }
 }
