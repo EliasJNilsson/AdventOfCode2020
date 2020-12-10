7fe671689379292311a8c2d0a8208c5801635b74
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-09 17:28:31 -0800

    Day 9 Part 2

--- Advent Of Code 2020/Day9/Program.cs
+++ Advent Of Code 2020/Day9/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day9
 {
@@ -17,6 +18,10 @@ namespace Day9
             }
 
             var FoundNumber = Part1(doubleList);
+            Console.WriteLine($"Found the odd one out: {FoundNumber}");
+
+            var finalNumber = Part2(FoundNumber, doubleList);
+            Console.WriteLine($"Found the Sum, first and last add to {finalNumber}");
         }
 
         public static double Part1(List<double> input)
@@ -33,7 +38,6 @@ namespace Day9
                 }
                 else
                 {
-                    Console.WriteLine($"Found the odd one out: {check}");
                     return check;
                 }
             }
@@ -52,6 +56,34 @@ namespace Day9
             }
 
             return false;
+        }
+
+        public static double Part2(double foundNumber, List<double> fullList)
+        {
+            var startIndex = 0;
+            var endIndex = 0;
+            double sum = 0;
+
+            foreach(var num in fullList)
+            {
+                sum += num;
+            }
+
+            var fullArr = fullList.ToArray();
+
+            while ((sum = fullArr[startIndex..endIndex].Sum()) != foundNumber)
+            {
+                if (sum > foundNumber) startIndex++;
+                if (sum < foundNumber) endIndex++;
+            }
+
+            var range = endIndex - startIndex;
+
+            var endList = fullList.GetRange(startIndex, range);
+
+            endList.Sort();
+
+            return endList[0] + endList.Last();
         }
     }
 }
