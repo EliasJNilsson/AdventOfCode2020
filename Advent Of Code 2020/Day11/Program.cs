c084feea7cdd613bd346086b90483538ef11f612
Elias Nilsson <EliasJNilsson@gmail.com>
2020-12-14 12:36:56 -0800

    Day 11 Part 1 set up

---
+++ Advent Of Code 2020/Day11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
+
+            //Part 1
+            var out1 = Part1(input);
+
+        }
+
+        public static int Part1(string[] input)
+        {
+            var changes = false;
+
+            do
+            {
+                //Check for changes
+
+
+                //If changes: Make changes, else exit
+
+
+            } while (changes == true);
+
+
+            //count seats?
+
+            return 0;
+        }
+
+
+    }
+}
